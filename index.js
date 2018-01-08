6322fa73e4490bc81004c8044dea53cb665c7a1b
James Overbay <orangetoque13@gmail.com>
2018-01-08 00:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var animal = 'dog'
 
 function myAnimal() {
   return animal
+  var animal = 'cat'
 }
 
 function yourAnimal() {
@@ -9,6 +10,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+  var animal = 'cat'
 }
 
 function add2(n) {
